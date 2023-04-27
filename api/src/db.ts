36299704a988cb4ca25becf0661246b897a813f7
Diego Hernan Piola <diegohernanpiola@gmail.com>
2023-04-26 23:53:05 -0300

    se corrigio el error del archivo .env

--- api/src/db.ts
+++ api/src/db.ts
@@ -6,7 +6,7 @@ export const sequelize = new Sequelize({
   database: config.dbName,
   password: config.dbPassword,
   username: config.dbUser,
-  port: 5000,
+  port: 5432,
   storage: ":memory:",
   models: [__dirname + "/models"],
 });
